095559f8dec6dcfa4015321e17b28119bf22ca72
SunayaShivakumar <sshivak2@illinois.edu>
2016-12-09 11:16:36 +0000

    finished site

--- source_js/script.js
+++ source_js/script.js
@@ -1,9 +1,49 @@
 // Write any custom javascript functions here
 
-$(document).ready(function(){
-  
-  $('body').click(function(){
-    $(this).toggleClass('llama')
-  });
+var lastId,
+    topMenu = $(".menu"),
+    topMenuHeight = topMenu.outerHeight()+10,
+    menuItems = topMenu.find("a"),
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 500);
+  e.preventDefault();
+});
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }                   
+});
 
+$(document).ready(function(){
+	//smooth scrolling
+	$(function() {
+		$(this.hash).click(function(e) {
+			e.preventDefault();
+			$('html, body').animate({ scrollTop: $(this.hash).offset().top}, 800);
+		});
+	});
 });
